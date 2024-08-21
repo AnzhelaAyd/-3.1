96f6980c15eb1eb79c636f1852e6632e4146d602
AnzhelaAyd <angiemagomedova@yandex.com>
2024-08-21 23:43:56 +0300

    Update module_3_1.py

--- module_3_1.py
+++ module_3_1.py
@@ -11,6 +11,12 @@ def string_info(string):
     return (len(string), string.upper(), string.lower())
 
 
+def is_contains(string, list_to_search):
+    return (string in list_to_search)
+
+
 print(string_info('Copybara'))
 print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclinc']))
 print(calls)
