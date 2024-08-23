48ecfd732e45822d47605ba2b54a0705340a21e4
AnzhelaAyd <angiemagomedova@yandex.com>
2024-08-23 13:01:05 +0300

    Update module_3_1.py

--- module_3_1.py
+++ module_3_1.py
@@ -3,7 +3,7 @@ calls = 0
 
 def count_calls():
     global calls
-    calls = 1
+    calls = calls + 1
 
 
 def string_info(string):
@@ -12,7 +12,8 @@ def string_info(string):
 
 
 def is_contains(string, list_to_search):
-    return (string in list_to_search)
+    count_calls()
+    return (string.lower() in [s.lower() for s in list_to_search])
 
 
 print(string_info('Copybara'))
